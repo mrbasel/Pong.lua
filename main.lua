e3010633c901127e6c2ad39e425b5956690f1cda
Basel <baselalnassr8@gmail.com>
2024-11-12 22:53:24 +0300

    better ui

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ require('player')
 require('ball')
 require('game')
 
+font = love.graphics.newFont(64)
 math.randomseed(os.time())
 
 if arg[2] == "debug" then
@@ -22,5 +23,15 @@ function love.draw()
     love.graphics.rectangle("fill", game.ball.x, game.ball.y, BALL_WIDTH, BALL_HEIGHT)
     love.graphics.rectangle("fill", game.player1.x, game.player1.y, PLAYER_WIDTH, PLAYER_HEIGHT)
     love.graphics.rectangle("fill", game.player2.x, game.player2.y, PLAYER_WIDTH, PLAYER_HEIGHT)
-    love.graphics.print(game.player1.score .. ' - ' .. game.player2.score)
+    
+    love.graphics.print(tostring(game.player1.score), font, WINDOW_WIDTH / 4, 50, 0)
+    love.graphics.print(tostring(game.player2.score), font, WINDOW_WIDTH - WINDOW_WIDTH / 4, 50, 0)
+
+    -- dotted line
+    love.graphics.setLineWidth(10)
+    local i = 0
+    while i < WINDOW_HEIGHT do
+        love.graphics.line(WINDOW_WIDTH / 2, i, WINDOW_WIDTH / 2, i + 40)
+        i = i + 70
+    end
 end
